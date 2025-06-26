029bd0d23cc04db8b1b8630c1215121eaca9dbcd
skupina-onebond <skupina@onebond.tech>
2025-06-26 11:36:58 +0200

    Update ContractDetailDialog.tsx

--- src/components/ContractDetailDialog.tsx
+++ src/components/ContractDetailDialog.tsx
@@ -211,13 +211,6 @@ useEffect(() => {
                 'Nad 5 milionů Kč'
               }</p>
             </section>
-
-            {contract.additional_info && (
-              <section>
-                <h3 className="text-base font-semibold text-slate-900 mb-2 uppercase tracking-wide">Dodatečné informace</h3>
-                <p>{contract.additional_info}</p>
-              </section>
-            )}
           </div>
 
           {/* RIGHT */}
