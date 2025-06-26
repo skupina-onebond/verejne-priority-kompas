6bf6b781065d66a0cce6f95c9d5f1569e89f0b89
skupina-onebond <skupina@onebond.tech>
2025-06-26 12:58:16 +0200

    Update seznam.tsx

--- src/pages/seznam.tsx
+++ src/pages/seznam.tsx
@@ -89,7 +89,7 @@ const ContractListPage = () => {
   className="min-h-screen"
   style={{
     fontFamily: 'Open Sans, sans-serif',
-    background: 'linear-gradient(to bottom, #d8e4ed, #eaf1f6, #f6fafc, #ffffff)'
+    background: 'linear-gradient(to bottom,#d8e4ed, #edf3f8, #f5f8fc)'
   }}
 >   
   <div className="container mx-auto p-6 relative">
